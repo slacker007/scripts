dc1cabc605c12884482e07767428e29d8a86aab7
Keelyn Roberts <slacker007.dev@gmail.com>
2017-12-05 15:30:30 -0500

    Added Check for root

--- dockerSetup.sh
+++ dockerSetup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check For Root Privileges
+if [[ $EUID -ne 0 ]]; then
+	echo "Please run this script as root" 1>&2
+	exit 1
+fi
+
 echo "$(tput setaf 2)[DOCKER INSTALLATION]...$(tput sgr0)";
 
 echo "$(tput setaf 2)[Updating Packages]...$(tput sgr0)";
